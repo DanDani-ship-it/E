e874edfdf4e658e80904e501b1a41e97d59273b4
DanDani-ship-it <175363921+DanDani-ship-it@users.noreply.github.com>
2024-08-07 23:08:06 -0500

    E v2

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2KVOWVLT.mjs";import"./chunk-AEJQHZPS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+declare var bootstrap: any;
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'E';
+  constructor(private _sanitizer: DomSanitizer){}
+
+  getVideoIframe(url: string) {
+    var video, results;
+ 
+    if (url === null) {
+        return '';
+    }
+    results = url.match('[\\?&]v=([^&#]*)');
+    video   = (results === null) ? url : results[1];
+ 
+    return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
+}
+
+  onClick(){
+    const modalElement = document.getElementById('exampleModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
 }
